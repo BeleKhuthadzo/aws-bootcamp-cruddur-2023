5fb0f1b94b1e6238ca34588e2a6b31532197bb9b
BeleKhuthadzo <Kbele15@gmail.com>
2023-03-06 13:55:47 +0000

    X-RAY

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -20,14 +20,14 @@ import logging
 from time import strftime
 
 # Configuring Logger to Use CloudWatch
-# LOGGER = logging.getLogger(__name__)
-# LOGGER.setLevel(logging.DEBUG)
-# console_handler = logging.StreamHandler()
-# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
-# LOGGER.addHandler(console_handler)
-# LOGGER.addHandler(cw_handler)
-# LOGGER.info("some message")
-# LOGGER.info("test log")
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("some message")
+LOGGER.info("test log")
 
 # Honeycomb -- app.py updates
 from opentelemetry import trace
@@ -51,9 +51,12 @@ tracer = trace.get_tracer(__name__)
 # x-ray
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 app = Flask(__name__)
+
+# x-ray
+XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
@@ -113,7 +116,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  data = HomeActivities.run()
+  data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,7 +6,7 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run(logger):
-    #logger.info("HomeActivities")
+    logger.info("HomeActivities")
 # Add attributes to span
     with tracer.start_as_current_span("home-activities-mock-data"):
        span = trace.get_current_span()
