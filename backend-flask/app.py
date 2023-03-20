a9d0871993a925aa390d7615a4c58b0efed3c8d0
BeleKhuthadzo <Kbele15@gmail.com>
2023-03-20 15:18:56 +0000

    Token

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
 
 from services.home_activities import *
 from services.notifications_activities import *
@@ -145,6 +146,10 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities_home')
 def data_home():
+  app.logger.debug("AUTH HEADER")
+  app.logger.debug(
+    request.headers.get('Authorization')
+    ),
   data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
